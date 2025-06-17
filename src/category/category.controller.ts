869935c7f6d3e1c4c6c18d61f399ead27bb43f10
Mukhammad-amin-ai <aminahmadov123@gmail.com>
2025-06-17 21:09:20 +0500

    [+] categoru CRUD fully added

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, HttpCode, Param, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Category, CategoryDocument } from './schema/category.schema';
 import { CategoryDto } from './dto/category.dto';
+import { CategoryUpdateDto } from './dto/categoryUpdate.dto';
 
 @Controller('category')
 export class CategoryController {
   category: CategoryDto[];
+  categoryUpdate: CategoryUpdateDto[];
   constructor(
     @InjectModel(Category.name) private categoryModel: Model<CategoryDocument>
   ) {}
@@ -27,5 +38,17 @@ export class CategoryController {
   @HttpCode(201)
   createCategory(@Body() body: CategoryDto) {
     return this.categoryModel.create(body);
+  }
+
+  @Patch(':id')
+  @HttpCode(200)
+  updateCategory(@Param('id') id: string, @Body() data: CategoryUpdateDto) {
+    return this.categoryModel.findByIdAndUpdate(id, data, { new: true });
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  deleteCategory(@Param('id') id: string) {
+    return this.categoryModel.findByIdAndDelete(id, { new: true });
   }
 }
